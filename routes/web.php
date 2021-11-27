4e16a812595933639f68c23ce867eaf498ec5418
3A832057 <qoooppadsl@gmail.com>
2021-11-27 22:51:18 +0800

    設立三個路由，分別為/、/about、/news

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewController;
+use App\Http\Controllers\AboutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/news',[NewController::class,'index'])->name('news.index');
+Route::get('/about',[AboutController::class,'index'])->name('about.index');
